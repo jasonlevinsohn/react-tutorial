99212a726136bbfae5f486893a8bfc3772247168
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-02-02 17:17:38 -0500

    finished lesson 3

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,3 +9,6 @@ var Home =  React.createClass({
 		)
 	}
 });
+
+
+module.exports = Home;
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,8 +3,12 @@ var Main = require('../components/Main');
 var Home = require('../components/Home');
 var Router = require('react-router');
 var Route = Router.Route;
+var IndexRoute = Router.IndexRoute;
 
 module.exports = (
     <Route path="/" component={Main}>
+        
+        //IndexRoute is used for default route.
+        <IndexRoute component={Home} />
     </Route>
 )
