de991a4f0a6f504e3a1265521c1e0f47cc5a0c3e
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-02-08 17:45:39 -0500

    add Profile.js

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,13 +1,14 @@
 var React = require('react');
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var Profile = require('../components/Profiles');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
 
 module.exports = (
     <Route path="/" component={Main}>
-        
+        <Route path="profile/:username" component={Profile} />
         //IndexRoute is used for default route.
         <IndexRoute component={Home} />
     </Route>
