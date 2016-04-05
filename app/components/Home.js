0b94b5deffcb4bc87f50e7829bce1ff4a0c7700c
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-04-05 12:03:50 -0400

    finished lesson 17: Just did stateless functional components on Home.js

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-class Home extends React.Component {
-    render() {
-		return (
-			<h2 className="text-center">
-				Search by Github Username Above
-			</h2>	
-		)
-    }
+// Let's use Stateless functional components for this component
+// 1. Doesn't have state
+// 2. Doesn't have a render method
+
+const Home = () => {
+    return (
+        <h2 className="text-center">
+            Search by Github Username Above
+        </h2>	
+    )
+
 }
 
 export default Home;
